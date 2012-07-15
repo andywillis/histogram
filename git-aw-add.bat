b8ad80637e1916c0e8ec051d92a8a99b825625e2
andy <urbanwhaleshark@gmail.com>
2012-07-15 17:02:59 +0100

    Second

--- git-aw-add.bat
+++ git-aw-add.bat
@@ -1,2 +1,8 @@
+git config user.name 'andywillis'
 git add .
-git commit -m "Next commit"
+git commit -m
+git remote rm origin
+git remote add origin git@github-public:andywillis/histogram.git
+git push -u origin master
+
+git remote add origin git@github-public:andywillis/node_modules.git
--- git-uws-add.bat
+++ git-uws-add.bat
@@ -1,5 +1,6 @@
+git config user.name 'urbanwhaleshark'
 git add .
-git commit -m %1
+git commit -m 
 git remote rm origin
 git remote add origin git@github.com:urbanwhaleshark/histogram.git
 git push -u origin master
